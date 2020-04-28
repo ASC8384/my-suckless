3eff593cf883e0c17209806fa745590816461ab8
ASC_8384 <ASC_8384@foxmail.com>
2020-04-29 00:02:11 +0800

    Add: auto start

--- dwm.c
+++ dwm.c
@@ -196,6 +196,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1394,6 +1395,12 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	//system("cd ~/.dwm; ./autostart_blocking.sh");
+	system("cd ~/code/dwm/sh; ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2214,6 +2221,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
 	run();
 	if(restart) execvp(argv[0], argv);
 	cleanup();
