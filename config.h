40222daa617cff16af27d0e005b8ff2f19561f7e
ASC_8384 <ASC_8384@foxmail.com>
2020-05-05 20:06:54 +0800

    Add: 豎排列表

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
