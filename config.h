a1f56bdfe14d276d1745dbd7aaf53485befd6bdd
ASC_8384 <ASC_8384@foxmail.com>
2020-05-05 19:53:07 +0800

    Add: center

--- config.h
+++ config.h
@@ -2,9 +2,11 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=15"
+	"monospace:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
