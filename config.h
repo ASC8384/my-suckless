45adb9277e56ec6e56fbb6626d6d3dbf1cc9c58b
ASC_8384 <ASC_8384@foxmail.com>
2020-05-05 20:15:28 +0800

    Add: fuzzy match

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=18"
