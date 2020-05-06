a96f2aba2df1288eaf7a309830d63bcdcb09f775
ASC_8384 <ASC_8384@foxmail.com>
2020-05-06 16:58:05 +0800

    Add: systray support

--- config.h
+++ config.h
@@ -6,6 +6,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int rmaster            = 1;        /* 1 means master-area is initially on the right */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;   /* 0 means no systray */
 static const int viewontag			= 1;     /* Switch view on tag switch */
 static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=16" };//{ "monospace:size=10" };
 static const char dmenufont[]       = "UbuntuMono Nerd Font Mono:size=16";//"monospace:size=10";
