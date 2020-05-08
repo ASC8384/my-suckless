6d1892cba0bfeab0bcc0988dfb2b77ef10490ad6
ASC_8384 <ASC_8384@foxmail.com>
2020-05-08 18:12:56 +0800

    Add: shortcut for scratch-toggle

--- config.h
+++ config.h
@@ -89,6 +89,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	{ Mod1Mask,						XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
